af2d76619e25de3c9be2bec4d282c5e0e4768c54
sjww1410 <32131751+sjww1410@users.noreply.github.com>
2021-06-02 20:52:28 +0800

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,18 +1,21 @@
-module.exports = ({ env }) => ({
+odule.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi-mysql-test01'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'sjww7138'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'ec2-54-243-92-68.compute-1.amazonaws.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'dctkgf8h194tjm'),
+        username: env('DATABASE_USERNAME', 'aqdbltopzjzknw'),
+        password: env('DATABASE_PASSWORD', 'b03571cf9c5094f45a7ca0fea4aa559976d48de8346b023191a1645fa75781dd'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
       },
-      options: {}
+      options: {},
     },
   },
 });
